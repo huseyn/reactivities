644a35948520b8c07a38e3b59fd2538deff89282
Huseyn <huseyn.mikayil95@gmail.com>
2020-12-15 00:40:51 +0400

    Create IActivity interface and modify type of response data in App component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import { Header, Icon, List } from "semantic-ui-react";
 import axios from "axios";
+import { IActivity } from "../models/activity";
 
-class App extends Component {
-  state = {
+interface IState {
+  activities: IActivity[];
+}
+
+class App extends Component<{}, IState> {
+  readonly state: IState = {
     activities: [],
   };
 
   componentDidMount() {
-    axios.get("http://localhost:5000/api/activities").then((response) => {
+    axios.get<IActivity[]>("http://localhost:5000/api/activities").then((response) => {
       this.setState({
         activities: response.data,
       });
@@ -23,7 +28,7 @@ class App extends Component {
           <Header.Content>Reactivities</Header.Content>
         </Header>
         <List>
-          {this.state.activities.map((activity: any) => (
+          {this.state.activities.map((activity) => (
             <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
