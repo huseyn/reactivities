4520f5a813f7a18460bd09f134e4858a275794a0
Huseyn <huseyn.mikayil95@gmail.com>
2020-12-16 22:55:17 +0400

    Create ActivityForm component

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, List } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import ActivityDetail from "../details/ActivityDetail";
+import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
 interface IProps {
@@ -16,6 +17,7 @@ const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
       </Grid.Column>
       <Grid.Column width={6}>
         <ActivityDetail />
+        <ActivityForm />
       </Grid.Column>
     </Grid>
   );
--- client-app/src/features/activities/details/ActivityDetail.tsx
+++ client-app/src/features/activities/details/ActivityDetail.tsx
@@ -8,17 +8,15 @@ const ActivityDetail = () => {
       <Card.Content>
         <Card.Header>Title</Card.Header>
         <Card.Meta>
-          <span >Date</span>
+          <span>Date</span>
         </Card.Meta>
-        <Card.Description>
-          Description
-        </Card.Description>
+        <Card.Description>Description</Card.Description>
       </Card.Content>
       <Card.Content extra>
-          <Button.Group widths={2}>
-              <Button basic color='blue' content='Edit' />
-              <Button basic color='grey' content='Cancel' />
-          </Button.Group>
+        <Button.Group widths={2}>
+          <Button basic color="blue" content="Edit" />
+          <Button basic color="grey" content="Cancel" />
+        </Button.Group>
       </Card.Content>
     </Card>
   );
